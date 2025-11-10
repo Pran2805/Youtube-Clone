e544f85541ca1eabad8472e027ab0bf8fce31019
Pran2805 <pranavshinde.as@gmail.com>
2025-11-10 19:47:29 +0530

    feat: Prisma Database setup with postgres and User schema added Basic

---
+++ youtube-clone/prisma/migrations/20251110141101_user_init/migration.sql
@@ -0,0 +1,17 @@
+-- CreateTable
+CREATE TABLE "users" (
+    "id" TEXT NOT NULL,
+    "clerkId" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "avatar" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_clerkId_key" ON "users"("clerkId");
+
+-- CreateIndex
+CREATE INDEX "clerk_id_idx" ON "users"("clerkId");
